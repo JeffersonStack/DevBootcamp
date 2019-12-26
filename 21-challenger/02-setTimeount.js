b8d8d0bc38639fcdf05e97f1fe35356a4341f95c
Jefferson Teles <jefferson_stack@outlook.com>
2019-12-26 15:45:17 -0200

    Aula 21 - ClearTimeount & ClearInterval, inicio das atividades

--- 21-challenger/02-setTimeount.js
+++ 21-challenger/02-setTimeount.js
@@ -1,7 +1,27 @@
-(function() {
+(function(window, document) {
   "use strict";
+
   // setTimeount
-  setTimeout(function() {
-    console.log("Olá Mundo!!");
-  }, 40000);
-})();
+  // setTimeout(function() {
+  //   console.log("Olá Mundo!!");
+  // }, 40000);
+
+
+  let button = document.querySelector('[data-js="button"]');
+  let counter = 0;
+  let temporizador;
+  function timer() {
+    console.log('Timer:', counter++);
+    if(counter > 10) {
+      return;
+    }
+
+    temporizador = setTimeout(timer, 1000);
+  }
+
+  button.addEventListener('click', function(){
+    clearTimeout(temporizador);
+  }, false)
+
+  timer();
+})(window, document);
