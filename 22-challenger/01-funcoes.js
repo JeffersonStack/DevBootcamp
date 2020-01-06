ba01fdeb48842591451e78600da1a18f58317e29
Jefferson Teles <jefferson_stack@outlook.com>
2020-01-05 23:16:54 -0200

    Aula 22 - Propriedades e metodos de funcoes

---
+++ 22-challenger/01-funcoes.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict'
+
+    // Propriedades e Métodos de funções
+
+    /**
+     * .name
+     *
+     * Retorna o nome de uma função como uma string
+     */
+
+    /**
+     * .length
+     * 
+     * Quandtidade de paramentros que uma função pode receber
+     */
+
+    function myFunction(a, b, c) {
+        return a + b
+    }
+
+    console.log(myFunction.length) // 3 propriedades
+})()
