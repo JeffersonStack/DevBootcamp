4ab11c87bf1e313aa92813aff1057907ccdfe9f2
Jefferson Teles <jefferson_stack@outlook.com>
2020-01-06 23:31:38 -0200

    Aula 22 - métodos de funcoes toString e call

--- 22-challenger/01-funcoes.js
+++ 22-challenger/01-funcoes.js
@@ -15,9 +15,35 @@
      * Quandtidade de paramentros que uma função pode receber
      */
 
-    function myFunction(a, b, c) {
-        return a + b
+    function myFunction(a, b, c, d) {
+        // cod
     }
 
-    console.log(myFunction.length) // 3 propriedades
+    console.log(myFunction.length) // 4 propriedades
+
+    /**
+     * toString
+     */
+
+    const arr = [1, 2, 3, 4];
+    console.log(arr.toString()); // 1,2,3,4
+
+    console.log(myFunction.toString()); // retorna toda a funcao em formato de string
+
+    /**
+     * call
+     * 
+     * O método call() invoca uma função com um dado valor this  e argumentos passados individualmente.
+     */
+
+    function completeName(name, sobrenome) {
+        this.name = name;
+        this.sobrenome = sobrenome;
+
+        return this;
+    }
+
+    console.log(completeName()) // Jefferson
+    console.log(completeName.call(this)) // Jefferson
+
 })()
